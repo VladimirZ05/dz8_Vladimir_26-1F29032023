f2d1f341a4eafa69e174dbc5e9325c786820b8ea
VladimirZ05 <132680480+VladimirZ05@users.noreply.github.com>
2023-08-09 13:34:02 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React , {useState , useEffect} from 'react'
+
+const Timer = () => {
+  const [ seconds , setSeconds ] = useState(0);
+
+  useEffect(() =>{
+    const interval = setInterval(() => {
+      setSeconds(prevSeconds => prevSeconds + 1);
+        }, 1000 );
+    return () => {
+      clearInterval(interval)
+    };
+      }, []);
+  return (
+      <div>
+        <h1>Timer</h1>
+        <p>Seconds: {seconds}</p>
+      </div>
+  )
+};
+export default Timer;
